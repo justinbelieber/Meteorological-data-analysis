014e47a2e1caa5d3f472a25885c6e0f09b556a4d
Fyeward <58617814+Fyeward@users.noreply.github.com>
2020-07-01 15:34:25 +0800

    pandas experience

--- Data-cleaning.py
+++ Data-cleaning.py
@@ -3,11 +3,11 @@ import pandas as pd
 from datetime import datetime
 from dateutil import parser
 
-data_raw = pd.read_csv(r'D:\spark\thedata.csv', encoding='utf-8')
+data_raw = pd.read_csv(r'D:\GITHUB\Meteorological-data-analysis\thedata.csv', encoding='utf-8')
 
 data = data_raw.loc[:, ['DATE', 'TMAX', 'TMIN']]
 
 
 data = data[(pd.Series.notnull(data['TMAX'])) & (pd.Series.notnull(data['TMIN']))]
 #data.query("date.dt.day == 28 & date.dt.month == 6", inplace=True)
-data.to_csv(r'D:\spark\afterdata.csv', index=None)
+data.to_csv(r'D:\GITHUB\Meteorological-data-analysis\afterdata.csv', index=None)
