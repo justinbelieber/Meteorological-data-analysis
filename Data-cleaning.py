6ae451a095f7a66912ac09155d6ade6309fe0fd3
Fyeward <58617814+Fyeward@users.noreply.github.com>
2020-07-01 17:35:14 +0800

    UPDATE 18301114 2020/7/1

--- Data-cleaning.py
+++ Data-cleaning.py
@@ -1,13 +1,20 @@
 
 import pandas as pd
+import matplotlib.pyplot as plt
+
 from datetime import datetime
 from dateutil import parser
 
 data_raw = pd.read_csv(r'D:\GITHUB\Meteorological-data-analysis\thedata.csv', encoding='utf-8')
 
 data = data_raw.loc[:, ['DATE', 'TMAX', 'TMIN']]
-
+data['DATE']=data['DATE'].apply(parser.parse)
 
 data = data[(pd.Series.notnull(data['TMAX'])) & (pd.Series.notnull(data['TMIN']))]
-#data.query("date.dt.day == 28 & date.dt.month == 6", inplace=True)
-data.to_csv(r'D:\GITHUB\Meteorological-data-analysis\afterdata.csv', index=None)
+data.query("DATE.dt.day == 15", inplace=True)
+#data.to_csv(r'D:\GITHUB\Meteorological-data-analysis\afterdata.csv', index=None)
+
+plt.figure(figsize=(20, 20))
+
+plt.plot(data['DATE'], data['TMAX'])
+plt.show()
