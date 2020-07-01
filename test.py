3f4c77edbcfcf3dc319230a253bd5ef2c2471a54
wmy <1481018815@qq.com>
2020-07-01 16:43:55 +0800

    test spark

--- test.py
+++ test.py
@@ -1,10 +1,12 @@
-from pyspark import SparkContext
-
-sc = SparkContext('local', 'test')
-
-lines = sc.textFile("test1.txt")
-line3 = sc.parallelize([1, 2, 3, 4])
-sumCount = line3.aggregate((0, 0),
-                           (lambda acc, value: (acc[0] + value, acc[1] + 1)),
-                           (lambda acc1, acc2: (acc1[0] + acc2[0], acc1[1] + acc2[1])))
-print(sumCount[0] / float(sumCount[1]))
+import pandas as pd
+from datetime import datetime
+from dateutil import parser
+data_raw = pd.read_csv('2199268.csv', encoding='utf-8')
+data_raw['date'] = data_raw['DATE'].apply(parser.parse)
+data_raw['tmax'] = data_raw['TMAX'].astype(float)
+data_raw['tmin'] = data_raw['TMIN'].astype(float)
+data = data_raw.loc[:, ['date', 'tmax', 'tmin']]
+data = data[(pd.Series.notnull(data['tmax'])) & (pd.Series.notnull(data['tmin']))]
+data = data[(data['date'] >= datetime(1980, 1, 1)) & (data['date'] <= datetime(2016, 1, 1))]
+data.query("date.dt.day == 28 & date.dt.month ==6", inplace=True)
+data.to_csv('maxmin.csv', index=None)
